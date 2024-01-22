a2fe30c844bf9747d1a90faac4967adddd5ab143
Euan Cunningham <euan@Asus-Lap>
2024-01-23 06:10:26 +1100

    nav up

--- Trial react build/src/components/Navbar.jsx
+++ Trial react build/src/components/Navbar.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import './Navbar.css'
 
 function Navbar() {
-  return <div>Navbar</div>
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="logo">Diver</div>
+        <ul className="nav">
+          <li>Home</li>
+          <li>Services</li>
+          <li>About</li>
+        </ul>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar

2621f4a5412d04400d82f3342b97741ea1c39979
Euan Cunningham <euan@Asus-Lap>
2024-01-29 18:04:23 +1100

    boxes section adding imgs

--- Trial react build/src/components/Boxes.jsx
+++ Trial react build/src/components/Boxes.jsx
@@ -5,28 +5,37 @@ function Boxes() {
   return (
     <section className="boxes">
       <div className="container">
-        <div className="box">
-          <h2>Un-touched Locations</h2>
-          <p>
-            Dive in places most people have never been before, we have access to
-            dive sites not accessable to the public
-          </p>
+        <div className="box-sect">
+          <div className="box">
+            <h2>Un-touched Locations</h2>
+            <p>
+              Dive in places most people have never been before, we have access
+              to dive sites not accessable to the public
+            </p>
+          </div>
+          <img src="untouched.jpg" />
         </div>
 
-        <div className="box">
-          <h2>Variety of packages</h2>
-          <p>
-            With our differnt dive packages, we can cater to everyone and
-            different budgets
-          </p>
+        <div className="box-sect">
+          <div className="box"></div>
+          <div className="box">
+            <h2>Variety of packages</h2>
+            <p>
+              With our differnt dive packages, we can cater to everyone and
+              different budgets
+            </p>
+          </div>
         </div>
 
-        <div className="box">
-          <h2>Training</h2>
-          <p>
-            Never been diving before? no problem, our dive guides are fully
-            qualified and can train you while on-board
-          </p>
+        <div className="box-sect">
+          <div className="box">
+            <h2>Training</h2>
+            <p>
+              Never been diving before? no problem, our dive guides are fully
+              qualified and can train you while on-board
+            </p>
+          </div>
+          <div className="box"></div>
         </div>
       </div>
     </section>

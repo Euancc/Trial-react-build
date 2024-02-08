6df77baefd51269468ecbc1eb265f7016c9d4bf4
Euan Cunningham <euan@Asus-Lap>
2024-02-09 06:12:55 +1100

    Changed stylesheet, creating hamburger menu

--- Trial react build/src/components/Navbar.jsx
+++ Trial react build/src/components/Navbar.jsx
@@ -1,15 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
+  const [click, setclick] = useState(false)
+  const handleClick = () => setclick(!click)
+
   return (
     <nav className="navbar">
       <div className="container">
         <div className="logo">
-          <a href="#">Diver</a>
+          <Link to="/">Diver</Link>
         </div>
-        <ul className="nav">
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        {/* <ul className="nav">
           <li className="nav-button">
             <Link to="/">Home</Link>
           </li>
@@ -19,7 +25,7 @@ function Navbar() {
           <li>
             <Link to="/about">About</Link>
           </li>
-        </ul>
+        </ul> */}
       </div>
     </nav>
   )

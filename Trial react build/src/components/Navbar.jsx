a8aa6881cf920c76b75df47f871b5699512e96c7
Euan Cunningham <euan@Asus-Lap>
2024-02-12 06:15:37 +1100

    nav popup menu

--- Trial react build/src/components/Navbar.jsx
+++ Trial react build/src/components/Navbar.jsx
@@ -1,10 +1,23 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { Link } from 'react-router-dom'
 
 function Navbar() {
   const [click, setclick] = useState(false)
+  const [button, setButton] = useState(true)
+
   const handleClick = () => setclick(!click)
+  const closeMobileMenu = () => setclick(false)
+
+  const showButton = () => {
+    if (window.innerWidth <= 960) {
+      setButton(false)
+    } else {
+      setButton(true)
+    }
+  }
+
+  window.addEventListener('resize', showButton)
 
   return (
     <nav className="navbar">
@@ -15,17 +28,24 @@ function Navbar() {
         <div className="menu-icon" onClick={handleClick}>
           <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
         </div>
-        {/* <ul className="nav">
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
           <li className="nav-button">
-            <Link to="/">Home</Link>
+            <Link to="/" onClick={closeMobileMenu}>
+              Home
+            </Link>
           </li>
-          <li>
-            <Link to="/services">Services</Link>
+          <li className="nav-button">
+            <Link to="/services" onClick={closeMobileMenu}>
+              Services
+            </Link>
           </li>
-          <li>
-            <Link to="/about">About</Link>
+          <li className="nav-button">
+            <Link to="/about" onClick={closeMobileMenu}>
+              About
+            </Link>
           </li>
-        </ul> */}
+        </ul>
+        {button && <Button buttonStyle="btn--outline">SIGN UP</Button>}
       </div>
     </nav>
   )
